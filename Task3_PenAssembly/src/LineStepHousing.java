7bcf3817818c7ffd06c22631fa234891a1afceb5
RybakAndrey <andrey_0895@mail.ru>
2015-12-11 23:29:48 +0300

    Check gitignore

--- Task3_PenAssembly/src/LineStepHousing.java
+++
@@ -1,9 +0,0 @@
-
-public class LineStepHousing implements ILineStep{
-	public LineStepHousing(){
-        System.out.println("Created line step housing");
-    }
-    public IProductPart buildProductPart(){
-        return new ProductPartHousing();
-    }
-}
--- Task3_PenAssembly/src/ProductPartHousing.java
+++
@@ -1,6 +0,0 @@
-
-public class ProductPartHousing implements IProductPart {
-	public ProductPartHousing(){
-        System.out.println("Created product part housing");
-    }
-}
