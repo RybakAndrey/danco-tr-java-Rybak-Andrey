7155fbb39478fd342d603ba4f898b845954feb3f
RybakAndrey <andrey_0895@mail.ru>
2015-12-12 13:16:40 +0300

    Change array of words

--- Task1_StringArray/src/Main.java
+++ Task1_StringArray/src/Main.java
@@ -3,8 +3,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		String array[] = new String[]{"Андрей","делает","Сделал",
-									  "десятое","Первое","Задание для Для курсов Курсов"};
+		String array[] = new String[]{"One two","three Four","Five","Six","seven","eight","nine","Ten",
+									  "Long long long Long String With Upper upper case Case Words"};
 		
 		StringArrayUtil util = new StringArrayUtil();
 		
